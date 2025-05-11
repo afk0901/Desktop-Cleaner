e4f5f92051eb2aaa0ef4273f944950ed2286fe12
afk0901 <arnarfkr@gmail.com>
2025-05-11 21:44:59 +0000

    Sufficient move.py tests

--- tests/unit_tests/test_move_multiple_files_by_dir_contents.py
+++ tests/unit_tests/test_move_multiple_files_by_dir_contents.py
@@ -10,13 +10,6 @@ class TestMoveMultipleFilesByDirContents:
         self.setup_teardown.setup()
         self.source_dir = self.setup_teardown.get_source_dir_path()
         self.new_directory_name = self.setup_teardown.get_destination_dir_name()
-        self.directory_content = [
-            "file1.txt",
-            "file2.txt",
-            "file3.jpg",
-            "subdir1",
-            "subdir2",
-        ]
 
     def teardown_method(self):
         self.setup_teardown = setUpTearDown()
@@ -25,10 +18,32 @@ class TestMoveMultipleFilesByDirContents:
     @patch("move._safe_move")
     def test_successful_move_multiple_files(self, safe_move_mock):
 
+        directory_content = [
+            "file1.txt",
+            "file2.txt",
+            "file3.jpg",
+            "subdir1",
+            "subdir2",
+        ]
+
         move_multiple_files_by_dir_contents(
             self.source_dir,
             self.new_directory_name,
-            self.directory_content,
+            directory_content,
         )
 
-        assert safe_move_mock.call_count == len(self.directory_content)
+        assert safe_move_mock.call_count == len(directory_content)
+    
+    @patch("move._safe_move")
+    def test_successful_move_multiple_files_with_empty_directory_content(self, safe_move_mock):
+        
+        directory_content = []
+        
+        move_multiple_files = move_multiple_files_by_dir_contents(
+            self.source_dir,
+            self.new_directory_name,
+            directory_content,
+        )
+
+        assert safe_move_mock.call_count == 0
+        assert move_multiple_files == None
