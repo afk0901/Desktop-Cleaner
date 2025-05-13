fdcea8e2747057f54d8f0283970250da59e85e6c
afk0901 <arnarfkr@gmail.com>
2025-05-13 00:08:59 +0000

    Testing _organize_files

--- tests/integration_tests/test_desktop_organizer/test_organize_directory_content_by_extensions.py
+++ tests/integration_tests/test_desktop_organizer/test_organize_directory_content_by_extensions.py
@@ -10,18 +10,9 @@ class TestOrganizeDirectoryContentByExtensions:
         setup_tear_down = setUpTearDown()
         os.mkdir(setup_tear_down.get_test_dir_path())
         self.source_directory_path = setup_tear_down.get_test_dir_path()
-        self.source_directory_content = [
-            "test_file1.jpg",
-            "test_file2.png",
-            "test_file4.docx",
-            "test_file5.jpeg",
-            "test_file3.txt",
-            "test_file6.webp",
-            "test_file7.bmp",
-        ]
-        for file_name in self.source_directory_content:
-            file_path = Path(self.source_directory_path) / file_name
-            file_path.touch()
+        self.source_directory_content = setup_tear_down.get_source_directory_content()
+        setup_tear_down.initialize_test_dir(self.source_directory_path, 
+                                            self.source_directory_content)
 
     def teardown_method(self):
         setUpTearDown().teardown()
@@ -35,10 +26,10 @@ class TestOrganizeDirectoryContentByExtensions:
         )
         new_dir = Path(self.source_directory_path) / Path("images")
         new_dir_content = os.listdir(new_dir)
-        assert new_dir_content == [
+        assert new_dir_content.sort() == [
             "test_file1.jpg",
             "test_file2.png",
             "test_file5.jpeg",
             "test_file6.webp",
             "test_file7.bmp",
-        ]
+        ].sort()
--- tests/set_up_tear_down.py
+++ tests/set_up_tear_down.py
@@ -18,6 +18,29 @@ class setUpTearDown:
 
     def get_destination_dir_path(self):
         return self.test_dir / self.get_destination_dir_name()
+    
+    def get_source_directory_content(self):
+        return [
+            "test_file1.jpg",
+            "test_file2.png",
+            "test_file4.docx",
+            "test_file5.jpeg",
+            "test_file3.txt",
+            "test_file9.pdf",
+            "test_file6.webp",
+            "test_file0.doc",
+            "test_file10.odt",
+            "test_file12.csv", 
+            "test_file2.xlsx",
+            "test_file7.bmp",
+        ]
+    
+    def initialize_test_dir(self, source_directory_path: str, 
+                            source_directory_content: list = None):
+        
+        for file_name in source_directory_content:
+            file_path = Path(source_directory_path) / file_name
+            file_path.touch()
 
     def setup(self):
         Path(self.get_test_dir_path()).mkdir(exist_ok=True)
